d926e07ad4ac815490c710f924efbf9641178e31
rokaya magdi <rokaiamagdi@gmail.com>
2021-01-12 18:27:59 +0200

    create my form

--- src/Table.js
+++ src/Table.js
@@ -2,47 +2,47 @@ import React, { Component } from 'react'
 
 
 const TableHeader = () => {
-    return (
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Job</th>
-                <th>Remove</th>
-            </tr>
-        </thead>
-    )
+  return (
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Remove</th>
+      </tr>
+    </thead>
+  )
 }
 
 const TableBody = (props) => {
-    const rows = props.characterData.map((row, index) => {
-        return (
-            <tr key={index}>
-                <td>{row.name}</td>
-                <td>{row.job}</td>
-                <td>
-                    <button onClick={() => props.removeCharacter(index)}>Delete</button>
-                </td>
-            </tr>
-        )
-    })
+  const rows = props.characterData.map((row, index) => {
     return (
-        <tbody>
-            {rows}
-        </tbody>
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>Delete</button>
+        </td>
+      </tr>
     )
+  })
+  return (
+    <tbody>
+      {rows}
+    </tbody>
+  )
 }
 class Table extends Component {
-    render() {
+  render() {
 
-        return (
-            <table>
-                <TableHeader />
-                <TableBody characterData={this.props.characterData} 
-                removeCharacter={this.props.removeCharecter}/>
+    return (
+      <table>
+        <TableHeader />
+        <TableBody characterData={this.props.characterData}
+          removeCharacter={this.props.removeCharecter} />
 
-            </table>
-        )
-    }
+      </table>
+    )
+  }
 }
 
 export default Table
